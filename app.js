07fd08f09aebebf93085a6c416e4b56934c36558
JCSweet <45698957+JCSweet@users.noreply.github.com>
2023-10-16 15:02:12 -0600

    styling + js
    added styling and all buttons function other than "options"

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const p1Score = document.querySelector("#p1Score");
 const p2Score = document.querySelector("#p2Score");
-const p1ScoreBtn = document.querySelector("#p1ScoreBtn");
-const p2ScoreBtn = document.querySelector("#p2ScoreBtn");
+const p1ScoreBtnUp = document.querySelector("#p1ScoreBtnUp");
+const p1ScoreBtnDown = document.querySelector("#p1ScoreBtnDown");
+const p2ScoreBtnUp = document.querySelector("#p2ScoreBtnUp");
+const p2ScoreBtnDown = document.querySelector("#p2ScoreBtnDown");
 const resetBtn = document.querySelector("#resetBtn");
 const winConBtn = document.querySelector("#winConBtn");
 let p1ScoreValue = 0;
 let p2ScoreValue = 0;
 
-p1ScoreBtn.addEventListener("click", () => {
+p1ScoreBtnUp.addEventListener("click", () => {
     p1ScoreValue+=1;
     p1Score.innerHTML=p1ScoreValue;
 });
 
-p2ScoreBtn.addEventListener("click", () => {
+p2ScoreBtnUp.addEventListener("click", () => {
     p2ScoreValue+=1;
+    p2Score.innerHTML=p2ScoreValue;
+});
+
+p1ScoreBtnDown.addEventListener("click", () => {
+    p1ScoreValue-=1;
+    p1Score.innerHTML=p1ScoreValue;
+});
+
+p2ScoreBtnDown.addEventListener("click", () => {
+    p2ScoreValue-=1;
     p2Score.innerHTML=p2ScoreValue;
 });
 
@@ -23,4 +35,10 @@ resetBtn.addEventListener("click", () => {
     p1Score.innerHTML=p1ScoreValue;
     p2Score.innerHTML=p2ScoreValue;
 });
+
+optionsBtn.addEventListener("click", () => {
+   
+});
+
+
 
