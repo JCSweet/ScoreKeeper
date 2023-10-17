bd291eae2ea94d24f2d74639e000ab2f5fe21c3e
JCSweet <45698957+JCSweet@users.noreply.github.com>
2023-10-17 12:33:58 -0600

    added win functionality
    added selector for choosing winning score and js for when there is a winner.

--- app.js
+++ app.js
@@ -5,40 +5,85 @@ const p1ScoreBtnDown = document.querySelector("#p1ScoreBtnDown");
 const p2ScoreBtnUp = document.querySelector("#p2ScoreBtnUp");
 const p2ScoreBtnDown = document.querySelector("#p2ScoreBtnDown");
 const resetBtn = document.querySelector("#resetBtn");
-const winConBtn = document.querySelector("#winConBtn");
+const winCon = document.getElementById("winConSelect");
+const winConDisplay = document.querySelector("#winConText");
+const p1Box = document.querySelector("#player1");
+const p2Box = document.querySelector("#player2");
+const h1 = document.querySelector("h1");
+const minWinScore = 1;
+const maxWinScore = 100;
 let p1ScoreValue = 0;
 let p2ScoreValue = 0;
+let winConValue = "";
+let winningPlayer = "";
 
+// Select Options
+for (let i = minWinScore; i <= maxWinScore; i++) {
+  const opt = document.createElement("option");
+  opt.value = i;
+  opt.innerHTML = i;
+  winCon.appendChild(opt);
+}
+
+// Display Win Con
+function winConn(w) {
+  winConValue = w;
+  if (winConValue != "") {
+    winConDisplay.innerHTML = `Playing up to ${winConValue}.`;
+  } else {
+    winConDisplay.innerHTML = "";
+  }
+}
+
+// Click Up / Click Down Event Listeners
 p1ScoreBtnUp.addEventListener("click", () => {
-    p1ScoreValue+=1;
-    p1Score.innerHTML=p1ScoreValue;
+  p1ScoreValue += 1;
+  p1Score.innerHTML = p1ScoreValue;
+  console.log(winConValue);
+  if (winConValue != "" && p1ScoreValue >= winConValue) {
+    winningPlayer = "Player 1";
+    win(winningPlayer);
+  }
 });
 
 p2ScoreBtnUp.addEventListener("click", () => {
-    p2ScoreValue+=1;
-    p2Score.innerHTML=p2ScoreValue;
+  p2ScoreValue += 1;
+  p2Score.innerHTML = p2ScoreValue;
+  if (winConValue != "" && p2ScoreValue >= winConValue) {
+    winningPlayer = "Player 2";
+    win(winningPlayer);
+  }
 });
 
 p1ScoreBtnDown.addEventListener("click", () => {
-    p1ScoreValue-=1;
-    p1Score.innerHTML=p1ScoreValue;
+  p1ScoreValue -= 1;
+  p1Score.innerHTML = p1ScoreValue;
 });
 
 p2ScoreBtnDown.addEventListener("click", () => {
-    p2ScoreValue-=1;
-    p2Score.innerHTML=p2ScoreValue;
+  p2ScoreValue -= 1;
+  p2Score.innerHTML = p2ScoreValue;
 });
 
+// Reset Button
 resetBtn.addEventListener("click", () => {
-    p1ScoreValue=0;
-    p2ScoreValue=0;
-    p1Score.innerHTML=p1ScoreValue;
-    p2Score.innerHTML=p2ScoreValue;
+  p1ScoreValue = 0;
+  p2ScoreValue = 0;
+  h1.innerText = "Score Keeper";
+  p1Score.innerHTML = p1ScoreValue;
+  p2Score.innerHTML = p2ScoreValue;
+  p1Box.classList.remove("winner");
+  p2Box.classList.remove("winner");
 });
 
-optionsBtn.addEventListener("click", () => {
-   
-});
-
-
-
+// Game Win
+function win(p) {
+  let winMessage = `${p} wins!!!!`;
+  h1.innerText = winMessage;
+  if (p === "Player 1") {
+    p1Box.classList.add("winner");
+    
+  } else if (p === "Player 2") {
+    p2Box.classList.add("winner");
+  }
+}
